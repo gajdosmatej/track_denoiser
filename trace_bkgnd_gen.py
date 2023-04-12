27a8855969bcc694f2e473bebddfd7dd341ff89b
Matěj Gajdoš <matej.gajdos@cvut.cz>
2023-04-12 15:16:41 +0200

    trace_bkgnd_gen.py ... Changed dimensions of R3 space. The particles' velocities are now sampled from continuous distributions.

--- trace_bkgnd_gen.py
+++ trace_bkgnd_gen.py
@@ -2,28 +2,29 @@ import numpy
 import matplotlib.pyplot
 
 datapath = "./data/"
-dim_X, dim_Y, dim_Z = 12, 10, 20
+dim_X, dim_Y, dim_Z = 12, 10, 200
+direction_norms = numpy.array([0.4,0.4,1.2])	#factors which multiply each coordinate of direction unit vector (main aim is to increase velocity in Z direction in order to compensate dim_Z >> dim_X, dim_Y)
 signal_energy = 1
-noise_energy = 0.7
-energy_deposition_coeff = 0.1
-signal_change_direction_probability = 0.9
-noise_change_direction_probability = 0.6
-noise_tracks_num_range = (5,10)
+noise_energy = 3
+energy_deposition_coeff = 0.3
+signal_change_direction_probability = 0.8
+noise_change_direction_probability = 0.8
+noise_tracks_num_range = (8,16)
 
 def updateDirection(direction :numpy.ndarray):
-	'''Takes a 'direction' vector from {-1,0,1}^3 and changes one of its components.'''
-	index = numpy.random.randint(0,3)
-	if abs(direction[index]) == 1:	direction[index] = 0
-	else:	direction[index] = 1 if numpy.random.random() > 0.5 else -1
+	'''Randomly updates the direction vector.'''
+	direction += numpy.random.normal(loc=0, scale=0.3)
+	direction /= numpy.linalg.norm(direction)
+	direction *= direction_norms
 
 
 def addSignal(space :numpy.ndarray):
 	'''Adds one signal track into the input 3D array space.'''
-	position = (dim_X//2, dim_Y//2, dim_Z//2)	#the track begins in the middle of the space
-	direction = numpy.random.randint(-1,2, size=3)	#random moving direction from {-1,0,1}^3 
-	while 0 <= position[0] < dim_X and 0 <= position[1] < dim_Y and 0 <= position[2] < dim_Z:	#the track propagades until it moves out of the space boundaries
-		space[position[0], position[1], position[2]] = signal_energy
-		if numpy.random.random() > signal_change_direction_probability:	updateDirection(direction)
+	position = (dim_X/2, dim_Y/2, dim_Z/2)	#the track begins in the middle of the space
+	direction = numpy.random.uniform( low = (-1)*direction_norms, high = direction_norms, size=3)	#random moving direction
+	while 0 <= round(position[0]) < dim_X and 0 <= round(position[1]) < dim_Y and 0 <= round(position[2]) < dim_Z:	#the track propagades until it moves out of the space boundaries
+		space[round(position[0]), round(position[1]), round(position[2])] = signal_energy
+		if numpy.random.random() < signal_change_direction_probability:	updateDirection(direction)
 		position += direction
 
 
@@ -36,29 +37,29 @@ def addNoise(space :numpy.ndarray):
 		side = numpy.random.randint(0,6)	#the noise always starts on the boundary of the space
 		energy = noise_energy
 		if side == 0:
-			position = numpy.array( [0, numpy.random.randint(0, dim_Y), numpy.random.randint(0, dim_Z)] )
-			direction = numpy.array([1,0,0])
+			position = numpy.array( [0, numpy.random.randint(0, dim_Y), numpy.random.randint(0, dim_Z)], dtype=float )
+			direction = numpy.array([direction_norms[0],0.,0.])
 		elif side == 1:
-			position = numpy.array( [dim_X-1, numpy.random.randint(0, dim_Y), numpy.random.randint(0, dim_Z)] )
-			direction = numpy.array([-1,0,0])
+			position = numpy.array( [dim_X-1, numpy.random.randint(0, dim_Y), numpy.random.randint(0, dim_Z)], dtype=float )
+			direction = numpy.array([-direction_norms[0],0.,0.])
 		elif side == 2:
-			position = numpy.array( [numpy.random.randint(0, dim_X), 0, numpy.random.randint(0, dim_Z)] )
-			direction = numpy.array([0,1,0])
+			position = numpy.array( [numpy.random.randint(0, dim_X), 0, numpy.random.randint(0, dim_Z)], dtype=float )
+			direction = numpy.array([0.,direction_norms[1],0.])
 		elif side == 3:
-			position = numpy.array( [numpy.random.randint(0, dim_X), dim_Y-1, numpy.random.randint(0, dim_Z)] )
-			direction = numpy.array([0,-1,0])
+			position = numpy.array( [numpy.random.randint(0, dim_X), dim_Y-1, numpy.random.randint(0, dim_Z)], dtype=float )
+			direction = numpy.array([0.,-direction_norms[1],0.])
 		elif side == 4:
-			position = numpy.array( [numpy.random.randint(0, dim_X), numpy.random.randint(0, dim_Y), 0] )
-			direction = numpy.array([0,0,1])
+			position = numpy.array( [numpy.random.randint(0, dim_X), numpy.random.randint(0, dim_Y), 0], dtype=float )
+			direction = numpy.array([0.,0.,direction_norms[2]])
 		else:
-			position = numpy.array( [numpy.random.randint(0, dim_X), numpy.random.randint(0, dim_Y), dim_Z-1] )
-			direction = numpy.array([0,0,-1])
+			position = numpy.array( [numpy.random.randint(0, dim_X), numpy.random.randint(0, dim_Y), dim_Z-1], dtype=float )
+			direction = numpy.array([0.,0.,-direction_norms[2]])
 		while energy > 0:	#the particle moves until it loses all the energy or it moves out of the space
 			energy -= numpy.random.exponential( energy_deposition_coeff )
-			space[position[0], position[1], position[2]] = energy
-			if numpy.random.random() > noise_change_direction_probability:	updateDirection(direction)
+			space[round(position[0]), round(position[1]), round(position[2])] = energy
+			if numpy.random.random() < noise_change_direction_probability:	updateDirection(direction)
 			position += direction
-			if position[0] >= dim_X or position[1] >= dim_Y or position[2] >= dim_Z or position[0] < 0 or position[1] < 0 or position[2] < 0:	break
+			if round(position[0]) >= dim_X or round(position[1]) >= dim_Y or round(position[2]) >= dim_Z or round(position[0]) < 0 or round(position[1]) < 0 or round(position[2]) < 0:	break
 
 
 def show3D(space :numpy.ndarray):
@@ -106,6 +107,8 @@ def genAndDumpData(iterations :int):
 		for k in range(3):
 			data_noise[k].append(getProjection(space, k))
 
+		if i % 1000 == 0:	print(i, "/", iterations)
+
 	for k in range(3):
 		numpy.save(datapath + noise_names[k], data_noise[k])
 		numpy.save(datapath + signal_names[k], data_signal[k])
@@ -123,31 +126,34 @@ def showRandomData():
 
 	for _ in range(5):
 		index = numpy.random.randint(0, size-1)
-		fig, ax = matplotlib.pyplot.subplots(2,3)
-		ax[0,0].imshow(zy_projections[index], cmap='gray')
+		fig, ax = matplotlib.pyplot.subplots(3,2)
+		ax[0,0].imshow(zy_projections_sgnl[index], cmap='gray')
 		ax[0,0].set_xlabel("z")
 		ax[0,0].set_ylabel("y")
-		ax[0,1].imshow(zx_projections[index], cmap='gray')
-		ax[0,1].set_xlabel("z")
-		ax[0,1].set_ylabel("x")
-		ax[0,2].imshow(yx_projections[index], cmap='gray')
-		ax[0,2].set_xlabel("y")
-		ax[0,2].set_ylabel("x")
-
-		ax[1,0].imshow(zy_projections_sgnl[index], cmap='gray')
+		ax[1,0].imshow(zx_projections_sgnl[index], cmap='gray')
 		ax[1,0].set_xlabel("z")
-		ax[1,0].set_ylabel("y")
-		ax[1,1].imshow(zx_projections_sgnl[index], cmap='gray')
+		ax[1,0].set_ylabel("x")
+		ax[2,0].imshow(yx_projections_sgnl[index], cmap='gray')
+		ax[2,0].set_xlabel("y")
+		ax[2,0].set_ylabel("x")
+
+		ax[0,1].imshow(zy_projections[index], cmap='gray')
+		ax[0,1].set_xlabel("z")
+		ax[0,1].set_ylabel("y")
+		ax[1,1].imshow(zx_projections[index], cmap='gray')
 		ax[1,1].set_xlabel("z")
 		ax[1,1].set_ylabel("x")
-		ax[1,2].imshow(yx_projections_sgnl[index], cmap='gray')
-		ax[1,2].set_xlabel("y")
-		ax[1,2].set_ylabel("x")
+		ax[2,1].imshow(yx_projections[index], cmap='gray')
+		ax[2,1].set_xlabel("y")
+		ax[2,1].set_ylabel("x")
 		matplotlib.pyplot.show()
 
-genAndDumpData(10000)
-#showRandomData()
-#space = numpy.zeros( (dim_X, dim_Y, dim_Z) )
-#addSignal(space)
-#addNoise(space)
-#show3D(space)
+#genAndDumpData(20000)
+showRandomData()
+'''
+space = numpy.zeros( (dim_X, dim_Y, dim_Z) )
+addSignal(space)
+addNoise(space)
+show3D(space)
+'''
+#showProjections(space)
