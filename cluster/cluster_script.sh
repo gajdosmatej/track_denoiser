24c7d503031e15a2e1b4a29d9a5dc05a245d9a28
Matěj Gajdoš <matej.gajdos@cvut.cz>
2023-05-10 16:21:28 +0200

    cluster/ ... Small changes in cluster scripts

--- cluster/cluster_script.sh
+++ cluster/cluster_script.sh
@@ -8,11 +8,14 @@ DATADIR=/storage/praha1/home/gajdoma6
 
 test -n "$SCRATCHDIR" || { echo >&2 "Variable SCRATCHDIR is not set!"; exit 1; }
 
-cp -r $DATADIR/data $DATADIR/denoise_traces_cls.py $DATADIR/run_cls.py $SCRATCHDIR
+cp -r $DATADIR/data $DATADIR/cluster/denoise_traces_cls.py $DATADIR/cluster/run_cls.py $SCRATCHDIR
 cd $SCRATCHDIR
 
 singularity exec -B $SCRATCHDIR:/scratchdir --nv /cvmfs/singularity.metacentrum.cz/NGC/TensorFlow\:22.12-tf2-py3.SIF python /scratchdir/run_cls.py
 
-cp -r $SCRATCHDIR/CLS_MODEL $DATADIR/CLS_MODEL
+rm -r $DATADIR/MODEL
+mkdir $DATADIR/MODEL
+cp -r $SCRATCHDIR/CLS_MODEL $DATADIR/MODEL/CLS_MODEL
+cp $SCRATCHDIR/history.json $DATADIR/MODEL/history.json
 
 clean_scratch
--- cluster/run_cls.py
+++ cluster/run_cls.py
@@ -1,7 +1,7 @@
 from denoise_traces_cls import Model
 import numpy
 from tensorflow import keras
-#import pandas
+import json
 
 
 zx_projections = numpy.load("/scratchdir/data/data_noise_zx.npy")
@@ -12,11 +12,9 @@ zx_projections_sgnl = zx_projections_sgnl.reshape( (*zx_projections_sgnl.shape, 
 
 model_zx = Model()
 model_zx.model.compile(optimizer="adam", loss="binary_crossentropy")
-history = model_zx.model.fit(x = zx_projections[:15000], y = zx_projections_sgnl[:15000], shuffle=True, epochs=1, validation_data=(zx_projections[15000:17000], zx_projections_sgnl[15000:17000]))
+history = model_zx.model.fit(x = zx_projections[:15000], y = zx_projections_sgnl[:15000], shuffle=True, epochs=30, validation_data=(zx_projections[15000:17000], zx_projections_sgnl[15000:17000]))
 model_zx.save()
 
-#hist_df = pandas.DataFrame(history.history) 
- 
-#hist_json_file = 'history_CLS_MODEL.json' 
-#with open(hist_json_file, mode='w') as f:
-#	hist_df.to_json(f)
+hist_json_file = 'history.json' 
+with open("/scratchdir/history.json", mode='w') as f:
+	json.dump(history.history, f)
