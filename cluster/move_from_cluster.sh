512da4ca74d642799e40e168cca045645186861d
Matěj Gajdoš <matej.gajdos@cvut.cz>
2023-08-16 10:45:28 +0200

    cluster/ ... Model names and regression/classification can be specified in architectures_list.py

--- cluster/move_from_cluster.sh
+++ cluster/move_from_cluster.sh
@@ -1,16 +1,26 @@
+#!/bin/bash
+
 scp -r gajdoma6@tarkil.metacentrum.cz:/storage/plzen1/home/gajdoma6/models ./cluster_models
 scp -r gajdoma6@tarkil.metacentrum.cz:/storage/plzen1/home/gajdoma6/histories ./cluster_histories
 
-num_existing_models="$(ls ../models/3D | wc -l)"
-num_new_models="$(ls ./cluster_models | wc -l)"
+#num_existing_models="$(ls ../models/3D | wc -l)"
+#num_new_models="$(ls ./cluster_models | wc -l)"
 
-for (( i=0; i < $num_new_models; ++i ))
+#for (( i=0; i < $num_new_models; ++i ))
+#do
+#	name=$((num_existing_models+i))
+#	mkdir ../models/3D/$name
+#	mv ./cluster_models/$i ../models/3D/$name/model
+#	mv ./cluster_histories/$i.json ../models/3D/$name/history.json
+#done
+
+cd ./cluster_models
+for model in *;
 do
-	name=$((num_existing_models+i))
-	mkdir ../models/3D/$name
-	mv ./cluster_models/$i ../models/3D/$name/model
-	mv ./cluster_histories/$i.json ../models/3D/$name/history.json
+	mkdir ../../models/3D/"$model"
+	mv -vn "$model" ../../models/3D/"$model"/model
+	mv -vn ../cluster_histories/"$model".json ../../models/3D/"$model"/history.json
 done
-
+cd ..
 rm -fr ./cluster_models
 rm -fr ./cluster_histories
--- cluster/run_several_architectures.py
+++ cluster/run_several_architectures.py
@@ -4,30 +4,35 @@ import tensorflow
 import architectures_list
 import json
 
-def dataPairLoad(low_id :int, high_id :int):
+def dataPairLoad(low_id :int, high_id :int, is_regression :bool):
 		while True:
 			order = numpy.arange(low_id, high_id)
 			numpy.random.shuffle(order)
 			for id in order:
 				signal_batch = numpy.load("/data/simulated/3D/" + str(id) + "_signal_3d.npy")
 				noise_batch = numpy.load("/data/simulated/3D/" + str(id) + "_noise_3d.npy")
+				if not is_regression:
+					signal_batch = numpy.where(signal_batch>0.01, 1, 0)
 				for i in range(5000):
 					yield ( numpy.reshape(noise_batch[i], (12,14,208,1)), numpy.reshape(signal_batch[i], (12,14,208,1)))
 	
-def getDataset(low, high):
-	return tensorflow.data.Dataset.from_generator(lambda: dataPairLoad(low, high), output_signature =
+def getDataset(low :int, high :int, is_regression :bool):
+	return tensorflow.data.Dataset.from_generator(lambda: dataPairLoad(low, high, is_regression), output_signature =
 					(	tensorflow.TensorSpec(shape=(12,14,208,1), dtype=tensorflow.float16),
 						tensorflow.TensorSpec(shape=(12,14,208,1), dtype=tensorflow.float16))
-					).batch(50).prefetch(20)
+					).batch(100).prefetch(20)
 
 
-training_dataset = getDataset(0,10)
-validation_dataset = getDataset(11,13)
-counter = 0
-for model in architectures_list.ARCHITECTURES:
+for architecture in architectures_list.ARCHITECTURES:
+	is_regression = (architecture["mode"] == "regression")
+	training_dataset = getDataset(0,10, is_regression)
+	validation_dataset = getDataset(11,13, is_regression)
+
+	model = architecture["model"]
 	model.compile(optimizer="Adam", loss="binary_crossentropy")
-	history = model.fit(x = training_dataset, epochs=50, steps_per_epoch=10, validation_data=validation_dataset, validation_steps=10)
-	model.save("/scratchdir/models/" + str(counter))
-	json_file = open("/scratchdir/histories/" + str(counter) + ".json", "w")
+	history = model.fit(x = training_dataset, epochs=100, steps_per_epoch=10, validation_data=validation_dataset, validation_steps=10,
+						callbacks = [keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)])
+	name = architecture["name"] + ("_R" if architecture["mode"] == "regression" else "_C")
+	architecture["model"].save("/scratchdir/models/" + name)
+	json_file = open("/scratchdir/histories/" + name + ".json", "w")
 	json.dump(history.history, json_file)
-	counter += 1
