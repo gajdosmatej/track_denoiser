cc5f23fb29938a18c0491ea69b4a369a3ada3cf2
Matej <gajdos.matej@yahoo.com>
2023-09-27 15:43:17 +0200

    denoise_traces.py ... Added attention layer subclasses.

--- denoise_traces.py
+++ denoise_traces.py
@@ -18,6 +18,54 @@ def normalise(event :numpy.ndarray):
 	return event / M
 
 
+class SEAttention(keras.layers.Layer):
+	'''
+	Custom Keras Layer applying channelwise attention to a 3D tensor. 
+	'''
+
+	def __init__(self):
+		super().__init__()
+	
+	def call(self, x):
+		return keras.layers.multiply([x, self.getChannelAttention(x)])
+	
+	def build(self, input_shape):
+		channels = input_shape[-1]
+
+		self.avging = keras.layers.GlobalAveragePooling3D()
+		self.maxing = keras.layers.GlobalMaxPooling3D()
+
+		self.MLP_1 = keras.layers.Dense(channels//2, "relu")
+		self.MLP_2 = keras.layers.Dense(channels, "relu")
+
+	def getChannelAttention(self, x):
+		'''
+		Squeeze-Excitation returning channel attention for 3D tensor @x.
+		'''
+
+		return keras.layers.Activation("sigmoid")( keras.layers.Add()([	self.MLP_2(self.MLP_1(self.avging(x))),
+																		self.MLP_2(self.MLP_1(self.maxing(x)))]) )
+
+
+class SpatialAttention(keras.layers.Layer):
+	'''
+	Custom Keras Layer applying spatial attention to a 3D tensor.
+	'''
+
+	def __init__(self):
+		super().__init__()
+	
+	def call(self, x):
+		return keras.layers.multiply([x, self.getSpatialAttention(x)])
+	
+	def build(self, input_shape):
+		self.aggregation = keras.layers.Conv3D(padding="same", strides=(1,1,1), kernel_size=(1,1,1), filters=1, activation="relu")
+		self.conv = keras.layers.Conv3D(padding="same", strides=(1,1,1), kernel_size=(3,3,3), filters=1, activation="sigmoid")
+
+	def getSpatialAttention(self, x):
+		return self.conv( self.aggregation(x) )
+
+
 class ModelWrapper:
 	def __init__(self, model :keras.Model, model_name :str = "", threshold :float = None):
 		self.model = model
