6b2ead80904dcb21e07f117c4fd7ef18c1cccff7
Matěj Gajdoš <gajdos.matej@yahoo.com>
2025-07-28 14:29:41 +0200

    Created init.sh for folder structure generation.

---
+++ init.sh
@@ -0,0 +1,8 @@
+mkdir data
+mkdir data/simulated
+mkdir data/x17
+mkdir data/simulated/clean
+mkdir data/simulated/noisy
+mkdir data/x17/clean
+mkdir data/x17/noisy
+mkdir data/x17/gauge_backgrounds
