577ec63ead4433affb75dd7dc33946e107d62969
Matěj Gajdoš <gajdos.matej@yahoo.com>
2025-07-30 16:09:10 +0200

    Distinguished between init_structure.sh and init_structure_data.sh

---
+++ init_structure_data.sh
@@ -0,0 +1,15 @@
+mkdir data
+mkdir data/simulated
+mkdir data/x17
+mkdir data/simulated/clean
+mkdir data/simulated/noisy
+echo "#> Data directory structure created. Data will now be downloaded from MetaCentrum."
+cd ./data/x17/
+read -p "#> Enter your MetaCentrum username: " username
+scp $username@tarkil.metacentrum.cz:/storage/projects/utefx17/matej/data/* .
+for file in *.tar.gz; do tar -xzf $file; done
+echo "#> Data downloaded and extracted."
+ln -s clean_5sigma clean
+ln -s noisy_5sigma noisy
+rm ./*.tar.gz
+cd ../..
